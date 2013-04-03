e9f15d51cc4e2542f6fe557c851e99ff9e19bac5
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2013-04-03 12:27:00 +0400

    Use legacy nodejs

--- barbudos/application/attributes/default.rb
+++ barbudos/application/attributes/default.rb
@@ -1,2 +1,4 @@
 default["barbudos_application"]["port"]   = 3000
+override["nodejs"]["install_method"]      = "package"
+override["nodejs"]["legacy_packages"]     = true
 override["nginx"]["default_site_enabled"] = false
--- barbudos/application/metadata.rb
+++ barbudos/application/metadata.rb
@@ -1,5 +1,5 @@
 name    "barbudos_application"
-version "0.0.0"
+version "0.0.1"
 
 %w{ application_nodejs nginx mongodb }.each do |cb|
   depends cb
